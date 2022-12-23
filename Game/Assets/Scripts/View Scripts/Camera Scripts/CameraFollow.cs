05123565c22ac0d7bf25063154d77fb6be846a8a
MichaelManson <84944327+MichaelManson@users.noreply.github.com>
2022-12-24 00:37:54 +1030

    made prefabs, ray not finding object tagged platform for frounded

--- Game/Assets/Scripts/View Scripts/Camera Scripts/CameraFollow.cs
+++ Game/Assets/Scripts/View Scripts/Camera Scripts/CameraFollow.cs
@@ -8,10 +8,19 @@ public class CameraFollow : MonoBehaviour
     public float yOffset =1f;
     public Transform target;
 
+    public float maxYPos;
+    public float minYPos;
+    public float yPos;
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x,target.position.y + yOffset,-10f);
+        if (target.position.y < minYPos) yPos = minYPos;
+        else if (target.position.y > maxYPos) yPos = maxYPos;
+        else yPos =target.position.y + yOffset;
+
+
+        Vector3 newPos = new Vector3(0,yPos,-10f);
         transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
     }
 }
