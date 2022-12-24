2f35a5fe8560ef550d6f19e865a4a509967dfda5
gegunt <gegunt@gmail.com>
2022-12-24 10:39:17 +0500

    Merge branch 'Gegunt' into Integration
    Updated player controller script

--- Game/Assets/Scripts/Controller Scripts/Player Scripts/PlayerController.cs
+++ Game/Assets/Scripts/Controller Scripts/Player Scripts/PlayerController.cs
@@ -365,6 +365,30 @@ public class PlayerController : MonoBehaviour
         }
     #endregion
 
+    #region Save/Load
+
+    int currentLevelIndex;
+    public void LoadData(GameData data)
+    {
+        this.currentLevelIndex = data.currentLevelIndex;
+        this.transform.position = data.playerPosition;
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.currentLevelIndex = this.currentLevelIndex;
+        data.playerPosition = this.transform.position;
+    }
+
+    
+    private void GetCurrentLevelIndex()
+        //Getting level index
+    {
+        currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    #endregion
+
     public void OnEnable()
     {
         playerInput.Enable();
